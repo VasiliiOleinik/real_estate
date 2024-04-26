bee78fae7088945b8ad9f0786366354688ccd985
voliinyk-blackbird <goooneoooogle@gmail.com>
2024-04-26 23:04:30 +0200

    exeptions

--- app/frontend/src/api/Estates/index.ts
+++ app/frontend/src/api/Estates/index.ts
@@ -2,7 +2,7 @@ import axios from '@/utils/axios'
 
 export async function getRealEstates() {
   try {
-    const result = await axios.get('/estates')
+    const result = await axios.get('/estate')
 
     console.log(result)
   } catch (error) {
